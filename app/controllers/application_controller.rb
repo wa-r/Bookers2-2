9c22434391a7805c0a4ee945e4692218d5ce3e78
wa-r <nyuuuuun14@gmail.com>
2020-12-12 05:23:39 +0000

    deviseのUserモデル作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parametes, if: devise_contoroller?
+
+  protected
+
+  def configure_permitted_parametes
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
+
 end
